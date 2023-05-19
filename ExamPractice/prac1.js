9fed7ba06630ec6052ff77a7a1b1b319acad4c1f
rajeevthapa09 <rajeevthapa09@gmail.com>
2023-05-18 22:37:07 -0500

    lab66

---
+++ ExamPractice/prac1.js
@@ -0,0 +1,23 @@
+console.log('start');
+
+async function foo() {
+
+    return 'done!';
+
+}
+
+ 
+
+async function bar() {
+
+    console.log('inside bar - start');
+
+    console.log(await foo()); 
+
+    console.log('inside bar - end');
+
+}
+
+bar();
+
+console.log('end');
